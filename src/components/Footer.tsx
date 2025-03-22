b80ae981f7ce6788f0e919f56524603028d3adf9
Ariel Gallardo <ariel.gallardo.dev@gmail.com>
2025-03-21 23:55:26 -0300

    7-pagina-de-inicio-y-footer# Footer Social

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,21 @@
+import { Stack } from "@mui/material";
+import Image from "next/image";
+
 export default () => {
-    return (
-      <footer className="bg-gray-800 text-white p-4 text-center">
-        <p>© 2025 Mi Sitio. Todos los derechos reservados.</p>
-      </footer>
-    );
-}
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="bg-gray-800 text-white p-4 text-center md:flex justify-between">
+      <div className="md:flex">
+        <p>© {year} CargoPay™.</p>
+        <p>Todos los derechos reservados.</p>
+      </div>
+      <Stack direction={"row"} spacing={1} className="justify-center">
+        <Image key={"whatsapp"} width={30} height={1} src={'/images/whatsapp.svg'} alt="whatsapp"/>
+        <Image key={"gmail"} width={30} height={1} src={'/images/gmail.svg'} alt="gmail"/>
+        <Image key={"discord"} width={30} height={1} src={'/images/discord.svg'} alt="discord"/>
+        <Image key={"linkedin"} width={30} height={1} src={'/images/linkedin.svg'} alt="linkedin"/>
+      </Stack>
+    </footer>
+  );
+};
