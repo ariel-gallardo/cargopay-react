817e5487c55db2c7311fd1da8dabae6ba2ce122c
Ariel Gallardo <ariel.gallardo.dev@gmail.com>
2025-03-21 23:44:00 -0300

    7-pagina-de-inicio-y-footer# Pagina de inicio

---
+++ src/components/Opinion.tsx
@@ -0,0 +1,19 @@
+import OpinionType from "@/models/OpinionType";
+import { Avatar, Box, Paper, Rating, Stack, Typography } from "@mui/material";
+
+
+const Opinion : React.FC<OpinionType> = (data: OpinionType) => {
+    return (
+        <Paper className="flex-none p-3 w-full">
+            <Stack spacing={1} direction={"row"}>
+            <Box>
+                <Avatar className="m-auto">{data.name}</Avatar>
+                <Rating defaultValue={data.rate}/>
+            </Box>
+            <Typography className="m-auto">{data.description}</Typography>
+            </Stack>
+        </Paper>
+    );
+}
+
+export default Opinion;
