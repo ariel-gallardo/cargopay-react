7a4a72bd5c885154640af5b775714e701d43a074
Ariel Gallardo <ariel.gallardo.dev@gmail.com>
2025-03-25 11:56:28 -0300

    5-crear-vista-de-perfil-asociacion-de-tarjetas# Perfil Usuario

--- src/app/profile/(dashboard)/page.tsx
+++ src/app/profile/(dashboard)/page.tsx
@@ -1,9 +1,25 @@
-import { Box } from "@mui/material";
+import { Box, Button, Card, FormControl, FormLabel, Stack, TextField, Typography } from "@mui/material";
+import { PageContainer } from "@toolpad/core";
 
 export default () => {
     return (
-        <Box>
-            <p>Profile</p>
-        </Box>
+        <PageContainer>
+            <Stack alignItems={'center'}>
+            <Card elevation={5} variant="elevation" className="p-5 w-[80vw] lg:p-10 lg:w-[20vw]" >
+                    <Typography textAlign={'center'} variant="h5">Perfil</Typography>
+                    <Stack gap={2}>
+                        <FormControl>
+                            <FormLabel>Password</FormLabel>
+                            <TextField type="password" placeholder="********"></TextField>
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Re-Password</FormLabel>
+                            <TextField type="password" placeholder="********"></TextField>
+                        </FormControl>
+                        <Button variant="contained">Guardar cambios</Button>
+                    </Stack>
+                </Card>
+            </Stack>
+        </PageContainer>
     );
 }
