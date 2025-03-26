d4829071f29c966b18748d90f55053426074cc0b
Ariel Gallardo <ariel.gallardo.dev@gmail.com>
2025-03-26 12:28:05 -0300

    11-vercel-no-show-images# Fix Unix load files

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,10 +11,10 @@ const FooterComponent = () => {
         <p>Todos los derechos reservados.</p>
       </div>
       <Stack direction={"row"} spacing={1} className="justify-center">
-        <Image key={"whatsapp"} width={30} height={1} src={'/images/whatsapp.svg'} alt="whatsapp"/>
-        <Image key={"gmail"} width={30} height={1} src={'/images/gmail.svg'} alt="gmail"/>
-        <Image key={"discord"} width={30} height={1} src={'/images/discord.svg'} alt="discord"/>
-        <Image key={"linkedin"} width={30} height={1} src={'/images/linkedin.svg'} alt="linkedin"/>
+        <Image key={"whatsapp"} width={30} height={1} src={'/images/Whatsapp.svg'} alt="whatsapp"/>
+        <Image key={"gmail"} width={30} height={1} src={'/images/Gmail.svg'} alt="gmail"/>
+        <Image key={"discord"} width={30} height={1} src={'/images/Discord.svg'} alt="discord"/>
+        <Image key={"linkedin"} width={30} height={1} src={'/images/LinkedIn.svg'} alt="linkedin"/>
       </Stack>
     </footer>
   );
